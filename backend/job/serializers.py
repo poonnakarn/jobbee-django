4422a28b55f508f9e0fe21cca0bea537c4e51ab4
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-04-29 21:17:56 +0700

    Create CandidatesApplied serializer

--- backend/job/serializers.py
+++ backend/job/serializers.py
@@ -1,8 +1,18 @@
+from dataclasses import field
 from rest_framework import serializers
-from .models import Job
+from .models import Job, CandidatesApplied
 
 
 class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         fields = "__all__"
+
+
+class CandidatesAppliedSerializer(serializers.ModelSerializer):
+
+    job = JobSerializer()
+
+    class Meta:
+        model = CandidatesApplied
+        field = ("user", "resume", "appliedAt", "job")
