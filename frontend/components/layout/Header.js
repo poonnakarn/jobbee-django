fe5fd8cf9dfa9912a30e32ef3d88d475d30f890d
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-05-05 21:16:34 +0700

    Dropdown menu and diplay user in Header

--- frontend/components/layout/Header.js
+++ frontend/components/layout/Header.js
@@ -1,7 +1,12 @@
+import { useContext } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
+import AuthContext from '../../context/AuthContext'
+
 const Header = () => {
+  const { loading, user } = useContext(AuthContext)
+
   return (
     <div className='navWrapper'>
       <div className='navContainer'>
@@ -20,12 +25,47 @@ const Header = () => {
               <span>Post A Job</span>
             </button>
           </Link>
-
-          <Link href='/login'>
-            <button className='loginButtonHeader'>
-              <span>Login</span>
-            </button>
-          </Link>
+          {user ? (
+            <div className='btn dropdown ml-3'>
+              <a
+                className='btn dropdown-toggle mr-4'
+                id='dropDownMenuButton'
+                data-toggle='dropdown'
+                aria-haspopup='true'
+                aria-expanded='false'
+              >
+                <span>Hi, {user.first_name}</span>
+              </a>
+              <div
+                className='dropdown-menu'
+                area-aria-labelledby='dropDownMenuButton'
+              >
+                <Link href='/employer/jobs'>
+                  <button className='dropdown-item'>My Jobs</button>
+                </Link>
+                <Link href='/me/applied'>
+                  <button className='dropdown-item'>Jobs Applied</button>
+                </Link>
+                <Link href='/me'>
+                  <button className='dropdown-item'>Profile</button>
+                </Link>
+                <Link href='/upload/resume'>
+                  <button className='dropdown-item'>Upload Resume</button>
+                </Link>
+                <Link href='/'>
+                  <button className='dropdown-item text-danger'>Logout</button>
+                </Link>
+              </div>
+            </div>
+          ) : (
+            !loading && (
+              <Link href='/login'>
+                <button className='loginButtonHeader'>
+                  <span>Login</span>
+                </button>
+              </Link>
+            )
+          )}
         </div>
       </div>
     </div>
