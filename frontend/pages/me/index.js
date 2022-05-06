dadf6d2e0055583d71704e22f64e1fbdf0464d30
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-05-06 07:57:55 +0700

    Edit profile page

---
+++ frontend/pages/me/index.js
@@ -0,0 +1,10 @@
+import Layout from '../../components/layout/Layout'
+import UpdateProfile from '../../components/user/UpdateProfile'
+
+export default function UpdateProfilePage() {
+  return (
+    <Layout title='Update Your Profile'>
+      <UpdateProfile />
+    </Layout>
+  )
+}
