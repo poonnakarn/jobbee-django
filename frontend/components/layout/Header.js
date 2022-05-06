33996eaea345b334a8d49c1679107be8395142a2
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-05-06 18:08:18 +0700

    NewJob component

--- frontend/components/layout/Header.js
+++ frontend/components/layout/Header.js
@@ -20,7 +20,7 @@ const Header = () => {
           </div>
         </Link>
         <div className='btnsWrapper'>
-          <Link href='/employeer/jobs/new'>
+          <Link href='/employer/jobs/new'>
             <button className='postAJobButton'>
               <span>Post A Job</span>
             </button>
---
+++ frontend/pages/employer/jobs/new.js
@@ -0,0 +1,33 @@
+import Layout from '../../../components/layout/Layout'
+import NewJob from '../../../components/job/NewJob'
+
+import { isAuthenticatedUser } from '../../../utils/isAuthenticated'
+
+export default function JobsAppliedPage({ accessToken }) {
+  return (
+    <Layout title='Post a new Job'>
+      <NewJob jobs={accessToken} />
+    </Layout>
+  )
+}
+
+// Protected Route
+export async function getServerSideProps({ req }) {
+  const accessToken = req.cookies.access // get token from cookies
+
+  const authenticated = await isAuthenticatedUser(accessToken)
+
+  // Return redirect to prevent response from header
+  if (!authenticated) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: { accessToken },
+  }
+}
